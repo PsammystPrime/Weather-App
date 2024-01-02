e9102e6792890593a74c7cd9c02b9765e83d94d2
PsammystPrime <samuelprime384@gmail.com>
2024-01-02 22:52:29 +0300

    Update data view to the browser

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const input = document.querySelector('input');
-const btn = document.getElementById('submit');
+const submit = document.getElementById('submit');
 const statusPara = document.getElementById('status');
 const resultPara = document.getElementById('result');
 
-fetch ('https://api.weatherapi.com/v1/current.json?key=e05dad397b6a4d4e9f0171133233012&q=london', {mode: "cors"})
+
+
+fetch ('https://api.weatherapi.com/v1/current.json?key=e05dad397b6a4d4e9f0171133233012&q=Nairobi', {mode: "cors"})
 .then(function(response){
     return response.json()
 })
 .then(function(response){
-    console.log(response.current.condition.text)
+    resultPara.textContent = response.current.condition.text
 })
-.catch(console.error('error'))
+.catch(function(error){
+    console.log('error')
+})
