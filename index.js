aa1e4450aa163dd2ca01e60f1fcdc4a305ef6f1a
PsammystPrime <samuelprime384@gmail.com>
2024-01-02 23:00:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const submit = document.getElementById('submit');
 const statusPara = document.getElementById('status');
 const resultPara = document.getElementById('result');
 
+submit.addEventListener('click', checkWeather);
+function checkWeather(){
 
+    fetch (`https://api.weatherapi.com/v1/current.json?key=e05dad397b6a4d4e9f0171133233012&q=${input.value}`, {mode: "cors"})
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(response){
+        resultPara.textContent = `${input.value} : ` + ' ' + response.current.condition.text
+    })
+    .catch(function(error){
+        console.log('error')
+    })
+}
 
-fetch ('https://api.weatherapi.com/v1/current.json?key=e05dad397b6a4d4e9f0171133233012&q=Nairobi', {mode: "cors"})
-.then(function(response){
-    return response.json()
-})
-.then(function(response){
-    resultPara.textContent = response.current.condition.text
-})
-.catch(function(error){
-    console.log('error')
-})
