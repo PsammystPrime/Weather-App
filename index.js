f3a31a03c0b1c9ad7bed9ad85ef97df8889eb206
PsammystPrime <samuelprime384@gmail.com>
2024-01-03 01:57:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const time = document.getElementById('time');
 const city = document.getElementById('city');
 const region = document.getElementById('region');
 const country = document.getElementById('country');
+const image = document.querySelector('img');
 
 submit.addEventListener('click', checkWeather);
 function checkWeather(){
@@ -18,10 +19,24 @@ function checkWeather(){
     .then(function(response){
        console.log(response)        
         statusPara.textContent = 'Weather updated!';
-        resultPara.textContent = 'Forecast :' + ' ' +  response.current.condition.text;
+        let forecast = response.current.condition.text;
+        resultPara.textContent = 'Forecast :' + ' ' +  forecast;
        time.textContent = 'Local time :' + ' ' +  response.location.localtime
        region.textContent = 'Region :' + ' ' +   response.location.tz_id
        country.textContent = 'Country :' + ' ' +  response.location.country
+       if (forecast.includes('cloudy')) {
+        image.src = './weather/64x64/day/119.png'
+       }else
+        if (forecast.includes('Clear')) {
+        image.src = './weather/64x64/day/113.png' 
+       }else
+        if (forecast.includes('rain')) {
+        image.src = './weather/64x64/day/263.png'
+       }else
+       if (forecast.includes('Wind')){
+        image.src = './weather/64x64/day/377.png'
+       }
+       
     })
     .catch(function(err){
         console.log(err)
