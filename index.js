9adbe7b98086ffb401d7034c02b7e15756524786
PsammystPrime <samuelprime384@gmail.com>
2024-01-02 22:37:34 +0300

    Initiate declarations

---
+++ index.js
@@ -0,0 +1,4 @@
+const input = document.querySelector('input');
+const btn = document.getElementById('submit');
+const statusPara = document.getElementById('status');
+const resultPara = document.getElementById('result');
