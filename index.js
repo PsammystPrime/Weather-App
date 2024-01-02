3a5db220910390f9719af8b1fa2d0a3de3b3735e
PsammystPrime <samuelprime384@gmail.com>
2024-01-02 23:09:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const resultPara = document.getElementById('result');
 
 submit.addEventListener('click', checkWeather);
 function checkWeather(){
-
+statusPara.textContent = ''
     fetch (`https://api.weatherapi.com/v1/current.json?key=e05dad397b6a4d4e9f0171133233012&q=${input.value}`, {mode: "cors"})
     .then(function(response){
+        statusPara.textContent = 'Fetching data...'
         return response.json()
     })
     .then(function(response){
+        statusPara.textContent = `Weather for ${input.value} updated!`
         resultPara.textContent = `${input.value} : ` + ' ' + response.current.condition.text
     })
     .catch(function(error){
-        console.log('error')
+        console.log(error)
+        statusPara.textContent = `Failed to update the weather for ${input.value}`
     })
 }
 
