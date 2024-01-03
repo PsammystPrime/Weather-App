c94fc1d465d30cd950858015a9f688c9093d056a
PsammystPrime <samuelprime384@gmail.com>
2024-01-03 22:21:12 +0300

    Add temperature and weather icon conditionals

--- index.js
+++ index.js
@@ -7,23 +7,30 @@ const city = document.getElementById('city');
 const region = document.getElementById('region');
 const country = document.getElementById('country');
 const image = document.querySelector('img');
+const temperature = document.getElementById('temps');
 
 submit.addEventListener('click', checkWeather);
 function checkWeather(){
-    statusPara.textContent = '';
+    statusPara.textContent = 'Fetching data...';
+    country.textContent = '';
+    resultPara.textContent = '';
+    region.textContent = '';
+    image.src = '';
+
     fetch (`https://api.weatherapi.com/v1/current.json?key=e05dad397b6a4d4e9f0171133233012&q=${input.value}`, {mode: "cors"})
     .then(function(response){
         statusPara.textContent = 'Fetching data...';
         return response.json()
     })
     .then(function(response){
-       console.log(response)        
+       console.log(response)       
         statusPara.textContent = 'Weather updated!';
         let forecast = response.current.condition.text;
         resultPara.textContent = 'Forecast :' + ' ' +  forecast;
        time.textContent = 'Local time :' + ' ' +  response.location.localtime;
        region.textContent = 'Region :' + ' ' +   response.location.tz_id;
        country.textContent = 'Country :' + ' ' +  response.location.country;
+       temperature.textContent = 'Temperature :' + ' ' + response.current.temp_c + '°C';
        if (forecast.includes('cloudy')) {
         image.src = './weather/64x64/day/119.png'
        }else
@@ -35,12 +42,26 @@ function checkWeather(){
        }else
        if (forecast.includes('Wind')){
         image.src = './weather/64x64/day/377.png'
+       }else
+       if (forecast.includes('mist')){
+        image.src = './weather/64x64/day/350.png'
+       }else
+       if (forecast.includes('fog')){
+        image.src = './weather/64x64/day/335.png'
+       }else
+       if (forecast.includes('storm')){
+        image.src = './weather/64x64/day/395.png'
+       }else
+       if (forecast.includes('snow')){
+        image.src = './weather/64x64/day/311.png'
        }
        
     })
-    .catch(function(err){
-        console.log(err)
-        statusPara.textContent = `Failed to update the weather for ${input.value}`;
+    .catch(function(error){
+        console.log(error)
+        statusPara.textContent = `Failed to update the weather for ${input.value}`
+        region.textContent = 'No matching location found'
     });
+    
 };
 
